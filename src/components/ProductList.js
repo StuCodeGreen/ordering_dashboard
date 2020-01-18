8eec6148b1d763dd59615b64c237b8b5dd085da6
stucodegreen <m.kuznecovas@gmail.com>
2020-01-18 20:29:50 +0000

    update

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -7,27 +7,65 @@ class ProductList extends React.Component {
     super(props);
     this.state = {
       url: 'data/db.json',
-			products: null,
-			currentPage:1,
-			productsPerPage:6
+      products: [],
+      currentPage: 1,
+      productsPerPage: 4,
+      totalPages: 0,
+      paginated: []
     };
+
+    this.pagination = this.pagination.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+  pagination() {
+    const { products, currentPage, productsPerPage } = this.state;
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(
+      indexOfFirstProduct,
+      indexOfLastProduct
+    );
+    this.setState({ paginated: currentProducts });
+  }
+
+  tick() {
+    setInterval(() => {
+      this.setState(prevState => ({
+        currentPage: prevState.currentPage + 1
+      }));
+      if (this.state.currentPage > this.state.totalPages) {
+        this.setState({ currentPage: 1 });
+      }
+      this.pagination();
+    }, 10000);
   }
 
   async componentDidMount() {
     const res = await axios.get(this.state.url);
-    // console.log(res);
-    this.setState({ products: res.data });
+    let pages = 0;
+    for (var k in res) {
+      if (res.hasOwnProperty(k)) {
+        pages++;
+      }
+    }
+
+    this.setState({ products: res.data, totalPages: pages });
+    this.pagination();
+    this.tick();
   }
 
+  componentDidUnmount() {
+    clearInterval(this.tick);
+  }
+
+	
   render() {
-
-		
-
     return (
       <React.Fragment>
-        {this.state.products ? (
+        {this.state.paginated ? (
           <div>
-            {this.state.products.map(product => (
+            {this.state.paginated.map((product, index) => (
               <ProductItems
                 key={product.id}
                 id={product.id}
