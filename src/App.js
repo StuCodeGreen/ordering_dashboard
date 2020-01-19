ea8b281b2b9d591f51874c7c844d0ad2f0690190
Martynas <m.kuznecovas@gmail.com>
2020-01-19 22:14:34 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import ProductList from './components/ProductList';
+import React from "react";
+import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <div className="App">
-		<ProductList />
+      <ProductList />
     </div>
   );
 }
