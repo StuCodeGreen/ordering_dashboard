82d732be5f1d1a24e524eeea179ad7eba9ac5206
stucodegreen <m.kuznecovas@gmail.com>
2020-05-02 20:58:35 +0100

    update

--- back-end/src/product.js
+++ back-end/src/product.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.submit = (event) => {
+  const requestBody = JSON.parse(event.body);
+  const {
+    size,
+    colour,
+    product_status,
+    customer_initials,
+    product_name,
+    category,
+  } = requestBody;
+};
