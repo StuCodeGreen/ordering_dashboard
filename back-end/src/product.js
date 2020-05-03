ed71a15f06becf9edf8428904cb0fa0ae6415465
stucodegreen <m.kuznecovas@gmail.com>
2020-05-03 13:58:36 +0100

    submit handler

--- back-end/src/product.js
+++ back-end/src/product.js
@@ -3,13 +3,12 @@
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
 
-AWS.config.setPromisesDependency(require('bluebird'));
-
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.submit = (event) => {
   const requestBody = JSON.parse(event.body);
   const {
+    id,
     size,
     colour,
     product_status,
@@ -17,4 +16,68 @@ module.exports.submit = (event) => {
     product_name,
     category,
   } = requestBody;
+
+  if (
+    typeof size !== 'number' ||
+    typeof colour !== 'string' ||
+    typeof product_status !== 'string' ||
+    typeof product_name !== 'string' ||
+    typeof customer_initials !== 'string' ||
+    typeof category !== 'string'
+  ) {
+    console.error('Validation Failed');
+    callback(new Error('Validation error'));
+    return;
+  }
+
+  submitProduct(
+    productInfo({
+      id,
+      size,
+      colour,
+      product_status,
+      customer_initials,
+      product_name,
+      category,
+    })
+  )
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Unable to ${
+            id ? 'update' : 'submit'
+          } prdouct with name ${product_name}`,
+        }),
+      });
+    });
+};
+
+const submitProduct = (product) => {
+  console.log('Submiting product');
+  const prdouctInfo = {
+    TableName: process.env.PRODUCT_TABLE,
+    Item: product,
+  };
+  return dynamoDb
+    .put(productInfo)
+    .promise()
+    .then((res) => product);
+};
+
+const productInfo = ({ id, ...product }) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: id ? id : uuid.v4(),
+    submittedAt: timestamp,
+    updatedAt: timestamp,
+    ...product,
+  };
 };
