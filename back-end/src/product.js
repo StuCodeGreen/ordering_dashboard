3b2f65faff2ad76ff3475eb09d45c2b39f94720d
stucodegreen <m.kuznecovas@gmail.com>
2020-05-03 20:38:55 +0100

    update

--- back-end/src/product.js
+++ back-end/src/product.js
@@ -6,78 +6,84 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.submit = (event, context, callback) => {
-  const requestBody = JSON.parse(event.body);
-  const {
-    id,
-    size,
-    colour,
-    product_status,
-    customer_initials,
-    product_name,
-    category,
-  } = requestBody;
+	const requestBody = JSON.parse(event.body);
+	const {
+		id,
+		size,
+		colour,
+		product_status,
+		customer_initial,
+		product_name,
+		category,
+		product_image,
+	} = requestBody;
 
-  if (
-    typeof size !== 'number' ||
-    typeof colour !== 'string' ||
-    typeof product_status !== 'string' ||
-    typeof product_name !== 'string' ||
-    typeof customer_initials !== 'string' ||
-    typeof category !== 'string'
-  ) {
-    console.error('Validation Failed');
-    callback(new Error('Validation error'));
-    return;
-  }
+	if (
+		typeof size !== 'string' ||
+		typeof colour !== 'string' ||
+		typeof product_status !== 'string' ||
+		typeof product_name !== 'string' ||
+		typeof customer_initial !== 'string' ||
+		typeof product_image !== 'string' ||
+		typeof category !== 'string'
+	) {
+		console.error('Validation Failed');
+		callback(new Error('Validation error'));
+		return;
+	}
 
-  submitProduct(
-    productInfo({
-      id,
-      size,
-      colour,
-      product_status,
-      customer_initials,
-      product_name,
-      category,
-    })
-  )
-    .then((res) => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(res),
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: `Unable to ${
+	submitProduct(
+			productInfo({
+				id,
+				size,
+				colour,
+				product_status,
+				customer_initial,
+				product_name,
+				category,
+				product_image,
+			})
+		)
+		.then((res) => {
+			callback(null, {
+				statusCode: 200,
+				body: JSON.stringify(res),
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			callback(null, {
+				statusCode: 500,
+				body: JSON.stringify({
+					message: `Unable to ${
             id ? 'update' : 'submit'
           } product with name ${product_name}`,
-        }),
-      });
-    });
+				}),
+			});
+		});
 };
 
 const submitProduct = (product) => {
-  console.log('Submitting product');
-  const productInfo = {
-    TableName: process.env.PRODUCT_TABLE,
-    Item: product,
-  };
-  return dynamoDb
-    .put(productInfo)
-    .promise()
-    .then((res) => product);
+	console.log('Submitting product');
+	const productInfo = {
+		TableName: process.env.PRODUCT_TABLE,
+		Item: product,
+	};
+	return dynamoDb
+		.put(productInfo)
+		.promise()
+		.then((res) => product);
 };
 
-const productInfo = ({ id, ...product }) => {
-  const timestamp = new Date().getTime();
-  return {
-    id: id ? id : uuid.v4(),
-    submittedAt: timestamp,
-    updatedAt: timestamp,
-    ...product,
-  };
+const productInfo = ({
+	id,
+	...product
+}) => {
+	const timestamp = new Date().getTime();
+	return {
+		id: id ? id : uuid.v4(),
+		submittedAt: timestamp,
+		updatedAt: timestamp,
+		...product,
+	};
 };
