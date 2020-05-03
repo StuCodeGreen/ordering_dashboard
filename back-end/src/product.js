29288c3e7323af0356e0f3755e1037dd81fdc593
stucodegreen <m.kuznecovas@gmail.com>
2020-05-03 21:30:10 +0100

    update

--- back-end/src/product.js
+++ back-end/src/product.js
@@ -6,111 +6,112 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.submit = (event, context, callback) => {
-	const requestBody = JSON.parse(event.body);
-	const {
-		id,
-		size,
-		colour,
-		product_status,
-		customer_initial,
-		product_name,
-		category,
-		product_image,
-	} = requestBody;
+  const requestBody = JSON.parse(event.body);
+  const {
+    id,
+    size,
+    colour,
+    product_status,
+    customer_initial,
+    product_name,
+    category,
+    product_image,
+  } = requestBody;
 
-	if (
-		typeof size !== 'string' ||
-		typeof colour !== 'string' ||
-		typeof product_status !== 'string' ||
-		typeof product_name !== 'string' ||
-		typeof customer_initial !== 'string' ||
-		typeof product_image !== 'string' ||
-		typeof category !== 'string'
-	) {
-		console.error('Validation Failed');
-		callback(new Error('Validation error'));
-		return;
-	}
+  if (
+    typeof size !== 'string' ||
+    typeof colour !== 'string' ||
+    typeof product_status !== 'string' ||
+    typeof product_name !== 'string' ||
+    typeof customer_initial !== 'string' ||
+    typeof product_image !== 'string' ||
+    typeof category !== 'string'
+  ) {
+    console.error('Validation Failed');
+    callback(new Error('Validation error'));
+    return;
+  }
 
-	submitProduct(
-			productInfo({
-				id,
-				size,
-				colour,
-				product_status,
-				customer_initial,
-				product_name,
-				category,
-				product_image,
-			})
-		)
-		.then((res) => {
-			callback(null, {
-				statusCode: 200,
-				body: JSON.stringify(res),
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-			callback(null, {
-				statusCode: 500,
-				body: JSON.stringify({
-					message: `Unable to ${
+  submitProduct(
+    productInfo({
+      id,
+      size,
+      colour,
+      product_status,
+      customer_initial,
+      product_name,
+      category,
+      product_image,
+    })
+  )
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Unable to ${
             id ? 'update' : 'submit'
           } product with name ${product_name}`,
-				}),
-			});
-		});
+        }),
+      });
+    });
 };
 
 const submitProduct = (product) => {
-	console.log('Submitting product');
-	const productInfo = {
-		TableName: process.env.PRODUCT_TABLE,
-		Item: product,
-	};
-	return dynamoDb
-		.put(productInfo)
-		.promise()
-		.then((res) => product);
+  console.log('Submitting product');
+  const productInfo = {
+    TableName: process.env.PRODUCT_TABLE,
+    Item: product,
+  };
+  return dynamoDb
+    .put(productInfo)
+    .promise()
+    .then((res) => product);
 };
 
-const productInfo = ({
-	id,
-	...product
-}) => {
-	const timestamp = new Date().getTime();
-	return {
-		id: id ? id : uuid.v4(),
-		submittedAt: timestamp,
-		updatedAt: timestamp,
-		...product,
-	};
+const productInfo = ({ id, ...product }) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: id ? id : uuid.v4(),
+    submittedAt: timestamp,
+    updatedAt: timestamp,
+    ...product,
+  };
 };
 
 module.exports.list = (event, context, callback) => {
-	const params = {
-		TableName: process.env.PRODUCT_TABLE,
-		ProjectionExpression: 'id, size, colour, product_satus, customer_initial, product_name, category',
-	}
-	console.log('Scanning Product table.');
-	const onScan = (err, data) => {
-		if (err) {
-			console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
-			callback(err);
-		} else {
-			console.log('Scan succeeded.');
-			return callback(null, {
-				statusCode: 200,
-				headers: {
-					'Access-Control-Allow-Origin': '*',
-					'Access-Control-Allow-Credentials': true,
-				},
-				body: JSON.stringify({
-					products: data.Items,
-				}),
-			});
-		}
-	};
-	dynamoDb.scan(params, onScan);
+  const params = {
+    TableName: process.env.PRODUCT_TABLE,
+    ProjectionExpression:
+      'id, size, colour, product_status, customer_initial, product_name, category, product_image',
+  };
+  console.log('Scanning Product table.');
+  const onScan = (err, data) => {
+    if (err) {
+      console.log(
+        'Scan failed to load data. Error JSON:',
+        JSON.stringify(err, null, 2)
+      );
+      callback(err);
+    } else {
+      console.log('Scan succeeded.');
+      return callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({
+          products: data.Items,
+        }),
+      });
+    }
+  };
+  dynamoDb.scan(params, onScan);
 };
--- front-end/src/components/ProductList.js
+++ front-end/src/components/ProductList.js
@@ -6,194 +6,166 @@ import ProductPagination from './ProductPagination';
 import './ProductList.css';
 
 class ProductList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			url: 'https://6hgqu0b2te.execute-api.eu-west-2.amazonaws.com/dev/products',
-			data: [],
-			products: [],
-			paginated: [],
-			currentPage: 1,
-			productsPerPage: 4,
-			totalPages: 0
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      url:
+        'https://6hgqu0b2te.execute-api.eu-west-2.amazonaws.com/dev/products',
+      data: [],
+      products: [],
+      paginated: [],
+      currentPage: 1,
+      productsPerPage: 4,
+      totalPages: 0,
+    };
 
-		this.pagination = this.pagination.bind(this);
-		this.tick = this.tick.bind(this);
-		this.status = this.status.bind(this);
-		this.totalPageCount = this.totalPageCount.bind(this);
-		this.selectedPage = this.selectedPage.bind(this);
-	}
+    this.pagination = this.pagination.bind(this);
+    this.tick = this.tick.bind(this);
+    this.status = this.status.bind(this);
+    this.totalPageCount = this.totalPageCount.bind(this);
+    this.selectedPage = this.selectedPage.bind(this);
+  }
 
-	async componentDidMount() {
-		const res = await axios.get(this.state.url);
-		this.setState({
-			products: res.data.products,
-			data: res.data.products
-		});
-		this.totalPageCount(this.state.products);
-		this.pagination(this.state.products, this.state.currentPage);
-		this.tick();
-	}
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({
+      products: res.data.products,
+      data: res.data.products,
+    });
+    this.totalPageCount(this.state.products);
+    this.pagination(this.state.products, this.state.currentPage);
+    this.tick();
+  }
 
-	componentWillUnmount() {
-		clearInterval(this.tick);
-	}
+  componentWillUnmount() {
+    clearInterval(this.tick);
+  }
 
-	totalPageCount(products) {
-		const {
-			productsPerPage
-		} = this.state;
-		let pageCount = Math.round(products.length / productsPerPage);
-		this.setState({
-			totalPages: pageCount
-		});
-	}
+  totalPageCount(products) {
+    const { productsPerPage } = this.state;
+    let pageCount = Math.round(products.length / productsPerPage);
+    this.setState({
+      totalPages: pageCount,
+    });
+  }
 
-	pagination(products, currentPage) {
-		const {
-			productsPerPage
-		} = this.state;
-		const indexOfLastProduct = currentPage * productsPerPage;
-		const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-		const currentProducts = products.slice(
-			indexOfFirstProduct,
-			indexOfLastProduct
-		);
-		this.setState({
-			paginated: currentProducts
-		});
-	}
+  pagination(products, currentPage) {
+    const { productsPerPage } = this.state;
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(
+      indexOfFirstProduct,
+      indexOfLastProduct
+    );
+    this.setState({
+      paginated: currentProducts,
+    });
+  }
 
-	tick() {
-		setInterval(() => {
-			let counter = this.state.currentPage;
-			counter++;
-			if (this.state.currentPage >= this.state.totalPages) {
-				counter = 1;
-			}
-			this.pageIndication(counter);
-			this.setState({
-				currentPage: counter
-			});
-			this.pagination(this.state.products, this.state.currentPage);
-		}, 10000);
-	}
+  tick() {
+    setInterval(() => {
+      let counter = this.state.currentPage;
+      counter++;
+      if (this.state.currentPage >= this.state.totalPages) {
+        counter = 1;
+      }
+      this.pageIndication(counter);
+      this.setState({
+        currentPage: counter,
+      });
+      this.pagination(this.state.products, this.state.currentPage);
+    }, 10000);
+  }
 
-	pageIndication(event) {
-		let span = document.querySelectorAll('.pages');
-		let element = document.getElementById(event);
-		for (var i = 0; i < span.length; i++) {
-			span[i].classList.remove('active');
-		}
-		element.classList.add('active');
-	}
+  pageIndication(event) {
+    let span = document.querySelectorAll('.pages');
+    let element = document.getElementById(event);
+    for (var i = 0; i < span.length; i++) {
+      span[i].classList.remove('active');
+    }
+    element.classList.add('active');
+  }
 
-	selectedPage(event) {
-		this.pageIndication(event.target.id);
-		this.setState({
-			currentPage: Number(event.target.id)
-		});
-		this.pagination(this.state.products, Number(event.target.id));
-	}
+  selectedPage(event) {
+    this.pageIndication(event.target.id);
+    this.setState({
+      currentPage: Number(event.target.id),
+    });
+    this.pagination(this.state.products, Number(event.target.id));
+  }
 
-	filter(status) {
-		if (status === 'all') {
-			this.pagination(this.state.data, 1);
-			this.totalPageCount(this.state.data);
-			this.setState({
-				products: this.state.data
-			});
-		} else {
-			let filtered = this.state.data.filter(product =>
-				product.status.includes(status)
-			);
-			this.totalPageCount(filtered);
-			this.pagination(filtered, 1);
-			this.setState({
-				products: filtered
-			});
-		}
-	}
+  filter(status) {
+    if (status === 'all') {
+      this.pagination(this.state.data, 1);
+      this.totalPageCount(this.state.data);
+      this.setState({
+        products: this.state.data,
+      });
+    } else {
+      let filtered = this.state.data.filter((product) =>
+        product.status.includes(status)
+      );
+      this.totalPageCount(filtered);
+      this.pagination(filtered, 1);
+      this.setState({
+        products: filtered,
+      });
+    }
+  }
 
-	status(event) {
-		if (event.target.id === 'all') {
-			this.filter(event.target.id);
-		}
-		if (event.target.id === 'ready') {
-			this.filter(event.target.id);
-		}
-		if (event.target.id === 'onTheWay') {
-			this.filter(event.target.id);
-		}
-		if (event.target.id === 'queue') {
-			this.filter(event.target.id);
-		}
-		if (event.target.id === 'outOfStock') {
-			this.filter(event.target.id);
-		}
-	}
+  status(event) {
+    if (event.target.id === 'all') {
+      this.filter(event.target.id);
+    }
+    if (event.target.id === 'ready') {
+      this.filter(event.target.id);
+    }
+    if (event.target.id === 'onTheWay') {
+      this.filter(event.target.id);
+    }
+    if (event.target.id === 'queue') {
+      this.filter(event.target.id);
+    }
+    if (event.target.id === 'outOfStock') {
+      this.filter(event.target.id);
+    }
+  }
 
-	render() {
-		return ( < React.Fragment > {
-					this.state.paginated ? ( < div className = "dashboard" >
-						<
-						ProductFilter status = {
-							this.status
-						}
-						/> <div className = "products" > {
-						' '
-					} {
-						this.state.paginated.map(product => ( <
-							ProductItems key = {
-								product.id
-							}
-							id = {
-								product.id
-							}
-							name = {
-								product.product_name
-							}
-							category = {
-								product.category
-							}
-							size = {
-								product.size
-							}
-							colour = {
-								product.colour
-							}
-							status = {
-								product.status
-							}
-							initial = {
-								product.customer_initial
-							}
-							img = {
-								product.product_image
-							}
-							/>
-						))
-					} <
-					/div> <
-					ProductPagination currentPage = {
-						this.state.currentPage
-					}
-					totalPages = {
-						this.state.totalPages
-					}
-					selectedPage = {
-						this.selectedPage
-					}
-					/> < /
-					div >
-				): ( <
-					h4 > Loading... < /h4>
-				)
-			} <
-			/React.Fragment>
-	);
-}
+  render() {
+    return (
+      <React.Fragment>
+        {' '}
+        {this.state.paginated ? (
+          <div className="dashboard">
+            <ProductFilter status={this.status} />{' '}
+            <div className="products">
+              {' '}
+              {this.state.paginated.map((product) => (
+                <ProductItems
+                  key={product.id}
+                  id={product.id}
+                  name={product.product_name}
+                  category={product.category}
+                  size={product.size}
+                  colour={product.colour}
+                  status={product.product_status}
+                  initial={product.customer_initial}
+                  img={product.product_image}
+                />
+              ))}{' '}
+            </div>{' '}
+            <ProductPagination
+              currentPage={this.state.currentPage}
+              totalPages={this.state.totalPages}
+              selectedPage={this.selectedPage}
+            />{' '}
+          </div>
+        ) : (
+          <h4> Loading... </h4>
+        )}{' '}
+      </React.Fragment>
+    );
+  }
 }
 
 export default ProductList;
