4e30dd8874fcd862ab1c1ff0cf6874d5be520344
stucodegreen <m.kuznecovas@gmail.com>
2020-05-13 00:12:27 +0100

    refactoring

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,186 @@
-import React from 'react';
-import './App.css';
-import ProductList from './components/ProductList';
+import React, { useState, useEffect, useCallback, useReducer } from 'react';
+import axios from 'axios';
+import ProductItems from './components/ProductItems';
+import ProductFilter from './components/ProductFilter';
+import ProductPagination from './components/ProductPagination';
+import './components/ProductList.css';
 
 function App() {
+  const [url] = useState(
+    // 'https://6hgqu0b2te.execute-api.eu-west-2.amazonaws.com/dev/products'
+    'db/db.json'
+  );
+
+  const [state, setState] = useReducer((prev, next) => ({ ...prev, ...next }), {
+    url: ['db/db.json'],
+    data: [],
+    products: [],
+    paginated: [],
+    currentPage: 1,
+    productsPerPage: 4,
+    totalPages: 0,
+  });
+
+  // const [data, setData] = useState([]);
+  // const [products, setProducts] = useState([]);
+  // const [paginated, setPaginated] = useState([]);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [productsPerPage] = useState(4);
+  // const [totalPages, setTotalPages] = useState(0);
+
+  const totalPageCount = useCallback(
+    (products) => {
+      let pageCount = Math.round(products.length / state.productsPerPage);
+      setState({ totalPages: pageCount });
+    },
+    [state, setState]
+  );
+
+  const pagination = useCallback(
+    (products, currentPage) => {
+      const indexOfLastProduct = currentPage * state.productsPerPage;
+      const indexOfFirstProduct = indexOfLastProduct - state.productsPerPage;
+      const currentProducts = products.slice(
+        indexOfFirstProduct,
+        indexOfLastProduct
+      );
+
+      setState({ paginated: currentProducts });
+    },
+    [setState, state]
+  );
+
+  const pageIndication = useCallback((event) => {
+    let span = document.querySelectorAll('.pages');
+    let element = document.getElementById(event);
+    for (var i = 0; i < span.length; i++) {
+      span[i].classList.remove('active');
+    }
+    element.classList.add('active');
+  }, []);
+
+  const selectedPage = useCallback(
+    (event) => {
+      pageIndication(event.target.id);
+
+      setState({ currentPage: Number(event.target.id) });
+
+      pagination(state.products, Number(event.target.id));
+    },
+    [setState]
+  );
+
+  const filter = useCallback(
+    (status) => {
+      if (status === 'all') {
+        setState({ products: state.data });
+        pagination(state.data, 1);
+        totalPageCount(state.data);
+      } else {
+        let filtered = state.data.filter((product) =>
+          product.product_status.includes(status)
+        );
+        setState({ products: filtered });
+        setState({ currentPage: 1 });
+        totalPageCount(filtered);
+        pagination(filtered, 1);
+      }
+    },
+    [setState, state]
+  );
+
+  const status = useCallback(
+    (event) => {
+      if (event.target.id === 'all') {
+        filter(event.target.id);
+      }
+      if (event.target.id === 'ready') {
+        filter(event.target.id);
+      }
+      if (event.target.id === 'onTheWay') {
+        filter(event.target.id);
+      }
+      if (event.target.id === 'queue') {
+        filter(event.target.id);
+      }
+      if (event.target.id === 'outOfStock') {
+        filter(event.target.id);
+      }
+      pageIndication(1);
+    },
+    [filter, pageIndication]
+  );
+
+  const tick = useCallback(() => {
+    const interval = setInterval(() => {
+      let counter = state.currentPage;
+      counter++;
+      if (state.currentPage >= state.totalPages) {
+        counter = 1;
+      }
+      pageIndication(counter);
+
+      setState({ currentPage: counter });
+      pagination(state.products, state.currentPage);
+    }, 10000);
+  }, [state, setState]);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get(state.url);
+      setState({ products: res.data.products });
+      setState({ data: res.data.products });
+      pageIndication(state.currentPage);
+    }
+    console.log('useEffect 1');
+    fetchData();
+  }, [state.url]);
+
+  useEffect(() => {
+    totalPageCount(state.products);
+    pagination(state.products, state.currentPage);
+    const interval = tick();
+    console.log('useEffect 2');
+    return () => {
+      clearTimeout(interval);
+    };
+  }, [
+    // totalPageCount,
+    // pagination,
+    state.products,
+    // tick,
+    state.currentPage,
+  ]);
+
   return (
-    <div className="App">
-      <ProductList />
-    </div>
+    <React.Fragment>
+      {state.paginated ? (
+        <div className="dashboard">
+          <ProductFilter status={status} />
+          <div className="products">
+            {state.paginated.map((product) => (
+              <ProductItems
+                key={product.id}
+                id={product.id}
+                name={product.product_name}
+                category={product.category}
+                size={product.size}
+                colour={product.colour}
+                status={product.product_status}
+                initial={product.customer_initial}
+                img={product.product_image}
+              />
+            ))}
+          </div>
+          <ProductPagination
+            currentPage={state.currentPage}
+            totalPages={state.totalPages}
+            selectedPage={selectedPage}
+          />
+        </div>
+      ) : (
+        <h4> Loading... </h4>
+      )}
+    </React.Fragment>
   );
 }
 
