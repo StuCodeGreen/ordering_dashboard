5eb544b6fc2d7b0777c5384c69914b2e85a2529a
stucodegreen <m.kuznecovas@gmail.com>
2020-05-13 02:45:46 +0100

    refactoring

--- front-end/src/components/ProductFilter.js
+++ front-end/src/components/ProductFilter.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import './ProductFilter.css';
 
-export default class ProductFilter extends React.Component {
-  render() {
-    const { status } = this.props;
+export default function ProductFilter(props) {
+  const { status } = props;
 
-    return (
-      <div className="filter">
-        <input type="button" id="all" onClick={status} />
-        <label htmlFor="all">all</label>
+  return (
+    <div className="filter">
+      <input type="button" id="all" onClick={status} />
+      <label htmlFor="all">all</label>
 
-        <input type="button" id="ready" onClick={status} />
-        <label htmlFor="ready">ready to try</label>
+      <input type="button" id="ready" onClick={status} />
+      <label htmlFor="ready">ready to try</label>
 
-        <input type="button" id="onTheWay" onClick={status} />
-        <label htmlFor="onTheWay">on the way</label>
+      <input type="button" id="onTheWay" onClick={status} />
+      <label htmlFor="onTheWay">on the way</label>
 
-        <input type="button" id="queue" onClick={status} />
-        <label htmlFor="queue">in the queue</label>
+      <input type="button" id="queue" onClick={status} />
+      <label htmlFor="queue">in the queue</label>
 
-        <input type="button" id="outOfStock" onClick={status} />
-        <label htmlFor="outOfStock">out of stock</label>
-      </div>
-    );
-  }
+      <input type="button" id="outOfStock" onClick={status} />
+      <label htmlFor="outOfStock">out of stock</label>
+    </div>
+  );
 }
